471bf44ddb15eee5dcb58b06820138a9b144876e
SIDHANT <acharyasid1@gmail.com>
2023-07-10 16:19:30 +0545

    introduction page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,21 @@ import './intro.scss'
 function Intro() {
   return (
     <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets\viber_image_2020-02-21_01-06-09.jpg" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>HI There I'M</h2>
+          <h1>Sidhant Acharya</h1>
+          <h3>Computer Science <span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets\pexels-kelly-2833717.jpg" alt="" />
+        </a>
+      </div>
       
     </div>
   )
