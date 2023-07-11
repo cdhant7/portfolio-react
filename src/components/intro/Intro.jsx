8075d7b3acde4b44f7c759a0f1ff5c24088bbf50
SIDHANT <acharyasid1@gmail.com>
2023-07-11 14:13:34 +0545

    text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,21 @@
+import { useEffect , useRef } from 'react'
 import './intro.scss'
+import { init } from 'ityped';
 
 function Intro() {
+
+  const textRef=useRef()
+
+  useEffect(()=>{
+    init(textRef.current, {
+       showCursor: true,
+       backDelay:1500,
+       backspeed:60,
+        strings: ["Student", "Beginner" ,"React Developer"] ,
+      });
+   
+  },[]);
+
   return (
     <div className='intro' id='intro'>
       <div className="left">
@@ -12,7 +27,7 @@ function Intro() {
         <div className="wrapper">
           <h2>HI There I'M</h2>
           <h1>Sidhant Acharya</h1>
-          <h3>Computer Science <span></span></h3>
+          <h3>Computer  <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets\pexels-kelly-2833717.jpg" alt="" />
