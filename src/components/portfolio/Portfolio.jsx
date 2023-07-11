d12c5cb36be60fc0606ffe0af6381d2368f02af5
SIDHANT <acharyasid1@gmail.com>
2023-07-11 15:12:47 +0545

    portfoliopage

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,41 @@ import './portfolio.scss'
 function Portfolio() {
   return (
     <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ul>
+
+    <div className="container">
+      <div className="item">
+        <img src="https://images.unsplash.com/photo-1618424181497-157f25b6ddd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="https://images.unsplash.com/photo-1618424181497-157f25b6ddd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="https://images.unsplash.com/photo-1618424181497-157f25b6ddd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="https://images.unsplash.com/photo-1618424181497-157f25b6ddd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="https://images.unsplash.com/photo-1618424181497-157f25b6ddd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="https://images.unsplash.com/photo-1618424181497-157f25b6ddd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wJTIwY29tcHV0ZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+        <h3>Banking App</h3>
+      </div>
+    </div>
       
     </div>
   )
