95d4812cdeca1dbf0e3373a6e4c9da87486ec88b
SIDHANT <acharyasid1@gmail.com>
2023-07-14 13:48:52 +0545

    testimonial cards

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,6 +3,23 @@ import './testimonials.scss'
 function Testimonials() {
   return (
     <div className='testimonials' id='testimonials'>
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <img src="assets\pexels-martin-de-arriba-11506752.jpg" alt="" />
+            <img src="https://media.istockphoto.com/id/1317094068/photo/5-stars-lying-on-a-wooden-board-dutch-angle-quality-concept.jpg?s=1024x1024&w=is&k=20&c=ZfhfKOUxUTk2ikLYEuzV5jfZdDQVsQ2ugf2ZTErx2Mk=" alt="" />
+            <img src="assets\pexels-szabó-viktor-3227986.jpg" alt="" />
+          </div>
+          <div className="center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam rem excepturi consequuntur corrupti dolorum aliquam hic
+          </div>
+          <div className="bottom">
+            <h3>CDHANT</h3>
+            <h4>CEO of MY WORLD</h4>
+          </div>
+        </div>
+      </div>
       
     </div>
   )
