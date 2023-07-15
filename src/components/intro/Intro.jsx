d1e703c414eaf85ba5c470d91b315f8319d685fa
SIDHANT <acharyasid1@gmail.com>
2023-07-15 13:43:29 +0545

    contact page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,22 +2,20 @@ import { useEffect , useRef } from 'react'
 import './intro.scss'
 import { init } from 'ityped';
 
-function Intro() {
+export default function Intro() {
+  const textRef = useRef();
 
-  const textRef=useRef()
-
-  useEffect(()=>{
+  useEffect(() => {
     init(textRef.current, {
-       showCursor: true,
-       backDelay:1500,
-       backspeed:60,
-        strings: ["Student", "Beginner" ,"React Developer"] ,
-      });
-   
-  },[]);
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
 
   return (
-    <div className='intro' id='intro'>
+    <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
           <img src="assets\viber_image_2020-02-21_01-06-09.jpg" alt="" />
@@ -25,18 +23,17 @@ function Intro() {
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>HI There I'M</h2>
+          <h2>Hi There, I'm</h2>
           <h1>Sidhant Acharya</h1>
-          <h3>Computer  <span ref={textRef}></span></h3>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#portfolio">
           <img src="assets\pexels-kelly-2833717.jpg" alt="" />
         </a>
       </div>
-      
     </div>
-  )
+  );
 }
-
-export default Intro
 
